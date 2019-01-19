3b99d57b11a6d7773b4c03dc713a4b4d51174bb4
Kirill <beloglazov.me@yandex.ru>
2019-01-19 22:59:27 +0300

    Task3_2 done

--- Task3_2.hs
+++ Task3_2.hs
@@ -2,12 +2,45 @@ module Task3_2 where
 
 import Todo(todo)
 
+myR1 = RCons (RNil) 1
+myR2 = RCons (myR1) 2
+myR3 = RCons (myR2) 3
+
 data ReverseList a = RNil | RCons (ReverseList a) a
 
 rlistToList :: ReverseList a -> [a]
-rlistToList = todo
+rlistToList z = foldl (\x y -> y:x) [] z
 
 listToRList :: [a] -> ReverseList a
-listToRList = todo
+listToRList z = foldl (\x y -> RCons x y) RNil z
+
+instance Foldable ReverseList where
+  foldr _ z RNil                  = z
+  foldr f z (RCons reverseList x) = f x (foldr f z reverseList)
 
 -- Реализуйте классы Eq, Ord, Show, Monoid, Functor
+
+instance (Eq a) => Eq (ReverseList a) where
+    (==) RNil          RNil          = True 
+    (==) (RCons x1 y1) (RCons x2 y2) = x1 == x2 && y1 == y2
+    (==) _             _             = False
+
+instance (Ord a) => Ord (ReverseList a) where
+    (<=) x y = (<=) x' y'
+        where x' = rlistToList x
+              y' = rlistToList y
+
+instance (Show a) => Show (ReverseList a) where
+    show RNil        = "[]"
+    show (RCons x y) = show x ++ ", " ++ show y
+
+instance Monoid (ReverseList a) where
+    mempty  = RNil
+    mappend = (<>)
+
+instance Semigroup (ReverseList a) where
+  (<>) x y = foldr (\h t -> RCons t h) y x
+
+instance Functor ReverseList where
+    fmap _ RNil        = RNil
+    fmap f (RCons x y) = RCons (fmap f x) (f y)  
