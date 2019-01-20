0331568a63f1f0c04be9185bea15b64cb49d5908
Kirill <beloglazov.me@yandex.ru>
2019-01-20 22:38:43 +0300

    Task4_1 done

--- Task4_1.hs
+++ Task4_1.hs
@@ -5,3 +5,19 @@ module Task4_1 where
 data FunMonad a = FunMonad { fun :: String -> a }
 
 -- реализуйте классы `Functor`, `Applicative` и `Monad` для типа `FunMonad`
+
+instance Functor FunMonad where
+    fmap x (FunMonad y) = FunMonad (x . y)
+
+instance Applicative FunMonad where
+    pure x    = FunMonad (\_ -> x)
+    (<*>) x y = appFoo x y
+
+appFoo x y = do
+    x1 <- x
+    y1 <- y
+    return (x1 y1)
+
+instance Monad FunMonad where
+    return x             = FunMonad (\_ -> x)
+    (>>=) (FunMonad x) y = FunMonad (\z -> (fun $ y $ (x z)) $ z)  
